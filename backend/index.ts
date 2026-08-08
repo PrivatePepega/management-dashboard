a4b3bb8799f92233eb18d85bd686b6c383f136f7
privatepepega <privatepepega@yahoo.com>
2026-08-08 17:35:04 -0400

    basic backend setup

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,27 @@
 import "dotenv/config";
-import express from "express";
+import express, { request } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import type { Request, Response, NextFunction, Errback } from 'express'
+
+
+
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+app.get("/", async (req:Request, res:Response) => {
+    res.status(200).json({message: "server hit"})
+})
+
+
+app.use((err: Errback, req:Request, res:Response, next:NextFunction) => {
+    res.status(404).json({message: "Not Found"})
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
